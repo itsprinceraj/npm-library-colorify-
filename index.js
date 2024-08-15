41775a7be228ad138619b21c8cc625ec1d1d414d
devops-prince <dev.prince116@gmailcom>
2024-08-15 14:12:26 +0530

    library created

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 export class Log {
   static success(string) {
-    return `%c ${string}`, "color: green;";
+    return `\x1b[32m${string}\x1b[0m`; // Green text
   }
 
   static danger(string) {
-    return `%c ${string}`, "color: red;";
+    return `\x1b[31m${string}\x1b[0m`; // Red text
   }
 
   static info(string) {
-    return `%c ${string}`, "color: black; background: yellow;";
+    return `\x1b[30m\x1b[43m${string}\x1b[0m`; // Black text with yellow background
   }
 }
